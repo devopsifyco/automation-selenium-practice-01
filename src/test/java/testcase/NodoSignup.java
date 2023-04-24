c639ecde29a4f743d5805912ec0f32458a65cbff
Tran Phu <hieutran1386@gmail.com>
2023-04-24 11:25:28 +0700

    Sign up with code and compare error message

--- src/test/java/testcase/NodoSignup.java
+++ src/test/java/testcase/NodoSignup.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -18,18 +19,28 @@ public class NodoSignup {
         chromeoption.addArguments("--remote-allow-origins=*"); //version browse 111....
         chromeoption.addArguments("--disable-notifications");
 
+
         ChromeDriver driver = new ChromeDriver(chromeoption);
         driver.get("https://node-web.chainservices.info/");
         driver.manage().window().maximize();
+
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement Signup = wait.until(webDriver -> driver.findElement(By.className("btn-sign-up")));
         Signup.click();
 
+
         WebElement Email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@class='ant-input'])[5]")));
         Email.sendKeys("consiu86+2@gmail.com");
+        WebElement Code = driver.findElement(By.xpath("(//input[@class='ant-input'])[6]"));
+        Code.sendKeys("MPQLTIDODJPI");
         WebElement Submit = driver.findElement(By.xpath("(//button[@type='submit'])"));
         Submit.click();
+
+        WebElement Errorcontent = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".error-content")));
+        String expectedresult = "Email has been registered already";
+        String actualresult = Errorcontent.getText();
+        Assert.assertEquals(actualresult, expectedresult);
 
     }
 }
