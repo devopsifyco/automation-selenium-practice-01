428cb7564602f50c077d5c4545f2bf1ecc2132d8
Quang <baoquang789@gmail.com>
2023-06-02 15:52:02 +0700

    add login page test case

--- src/main/java/demowebshop_pages/LoginPage.java
+++ src/main/java/demowebshop_pages/LoginPage.java
@@ -1,11 +1,27 @@
 package demowebshop_pages;
 
 import base.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage extends BasePage {
 
+    private final By header = By.tagName("h1");
+    private final By tbxEmail = By.id("Email");
+    private final By tbxPassword = By.id("Password");
+    private final By btnLogin = By.className("button-1 login-button");
     public LoginPage(WebDriver driver) {
         super(driver);
+    }
+
+    public String getLoginPageTitle(){
+        return getPageTitle();
+    }
+
+    public void login(String sEmail, String sPassword)
+    {
+        enter(tbxEmail, sEmail);
+        enter(tbxPassword, sPassword);
+        clickElement(btnLogin);
     }
 }
---
+++ src/test/java/demowebshop_Tests/TC_002.java
@@ -0,0 +1,46 @@
+package demowebshop_Tests;
+
+import base.BaseTests;
+import base.Page;
+import demowebshop_pages.HomePage;
+import demowebshop_pages.LoginPage;
+import demowebshop_pages.RegisterPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import testcase.BaseTest;
+
+public class TC_002 extends BaseTests {
+    String expTitleLoginPage ="Demo Web Shop. Login";
+    String expTitleHomePage = "Demo Web Shop.";
+   // String expRegisterSuccMessage= "Your registration completed";
+
+  //  String expValidationError = "The specified email already exists";
+    TC001 t = new TC001();
+    LoginPage loginPage;
+
+/*    @Test(priority = 1)
+    public void verifyToHomePageSuccessfully() throws Exception {
+        String actualTitle = page.getInstance(HomePage.class).getHomePageTitle();
+        Assert.assertEquals(actualTitle,expTitleHomePage);
+    }*/
+    @Test(priority = 1)
+    public void verifyNavigateToLoginPageSuccessfully() throws Exception {
+        t.verifyToHomePageSuccessfully();
+       /* String actualTitle = page.getInstance(LoginPage.class).getLoginPageTitle();
+        Assert.assertEquals(actualTitle, expTitleLoginPage);*/
+        loginPage = page.getInstance(HomePage.class).navigateToLoginPage();
+        Assert.assertEquals(loginPage.getLoginPageTitle(),expTitleLoginPage);
+    }
+
+    @Test(priority = 2)
+    public void verifyLoginSuccessfully()
+    {
+        //
+    }
+
+/*    @Test(priority = 4)
+    public void verifyToRegisterExistingAccout() {
+        registerPage.registerInfor("Huyen","Tran","consiu862@gmail.com","123@QWa4");
+        Assert.assertEquals(registerPage.getValiationError(), expValidationError);
+    }*/
+}
